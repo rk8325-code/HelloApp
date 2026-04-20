685242a15ded5bfcd03316a3c2a619ce15e9531a
Rithvik <rk8325@srmist.edu.in>
2026-04-20 08:33:33 +0530

    UC3 implemented

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Check if a name was provided
+        // Default name
+        String name = "World";
+
+        // Check if a name is provided as a command-line argument
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello World");
+            name = args[0]; // Use the provided name
         }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
