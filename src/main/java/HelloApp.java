81e35d414f65478130fc9ee1a12273cd7f5d5739
Rithvik <rk8325@srmist.edu.in>
2026-04-20 08:40:04 +0530

    UC4 implemented

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,30 @@
+/**
+ * HelloApp – UC4 – A simple Java application that greets multiple users by name if
+ * provided as command-line arguments, or defaults to greeting "World" if no names are given.
+ *
+ * Usage: java HelloApp [name1] [name2] ... [nameN]
+ * - If names are provided, it will display "Hello, [Name1], [Name2], ...!"
+ * - If no names are provided, it will display "Hello, World!"
+ *
+ * @author Developer Name
+ * @version 4.0
+ */
+
 public class HelloApp {
     public static void main(String[] args) {
-        // Default name
-        String name = "World";
-
-        // Check if a name is provided as a command-line argument
-        if (args.length > 0) {
-            name = args[0]; // Use the provided name
+        // Default greeting
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Build a greeting string from multiple names
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
         }
-
-        System.out.println("Hello, " + name + "!");
     }
 }
