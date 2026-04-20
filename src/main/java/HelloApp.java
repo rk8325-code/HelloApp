8f085002ea3da75f8b19911916c05e4a8e2aeb25
Rithvik <rk8325@srmist.edu.in>
2026-04-20 08:55:56 +0530

    UC7 implemented

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,19 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Default greeting if no arguments are passed
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            // Build greeting string with trailing delimiter
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
+        // Default greeting
+        String names = "World";
 
-            // Remove the last ", " using substring
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            System.out.println("Hello, " + names + "!");
+        // If arguments are provided, join them with commas
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
