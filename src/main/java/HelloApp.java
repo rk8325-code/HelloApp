8ba65cbab7ee647e94625e506031220a0f9c5e1c
Rithvik <rk8325@srmist.edu.in>
2026-04-20 08:25:53 +0530

    UC2 implemented

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,8 +1,11 @@
-
-
 public class HelloApp {
-public static void main(String[] args) {
-System.out.println("Hello World");
-}
-
+    public static void main(String[] args) {
+        // Check if a name was provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello World");
+        }
+    }
 }
